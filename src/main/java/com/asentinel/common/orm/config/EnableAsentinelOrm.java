ca078f0d09d2ce0418f6084b39907eb3ba406a71
Razvan.Popian <Razvan.Popian@tangoe,com>
2026-01-20 18:04:37 +0200

    more config

--- src/main/java/com/asentinel/common/orm/config/EnableAsentinelOrm.java
+++ src/main/java/com/asentinel/common/orm/config/EnableAsentinelOrm.java
@@ -14,39 +14,9 @@ import com.asentinel.common.orm.OrmOperations;
  * Enables Asentinel ORM support. Creates the central ORM interface
  * {@link OrmOperations} bean that can be then injected into other beans. Client
  * code is responsible for providing the following beans:
- * <li>{@code JdbcFlavor}
- * <li>{@code JdbcOperations} built on top of a configured {@code DataSource}
- * <li>{@code OrmConversionServiceConfig} is optional for registering custom
- * converters.<br>
- * 
- * <br>
- * <br>
- * 
- * Example:
- * 
- * <pre>
- * &#64;Bean
- * public DataSource dataSource() {
- * 	return new SingleConnectionDataSource("jdbc:h2:mem:testdb", "sa", "", false);
- * }
- * 
- * &#64;Bean
- * public JdbcFlavor jdbcFlavor() {
- * 	return new H2JdbcFlavor();
- * }
- * 
- * &#64;Bean
- * public JdbcOperations jdbcOperations(DataSource dataSource, JdbcFlavor jdbcFlavor) {
- * 	return new JdbcTemplate(dataSource) {
- * 
- * 		// add support for byte[], InputStream and Enum params
- * 		&#64;Override
- * 		protected PreparedStatementSetter newArgPreparedStatementSetter(Object[] args) {
- * 			return new CustomArgumentPreparedStatementSetter(jdbcFlavor, args);
- * 		}
- * 	};
- * }
- * </pre>
+ * <li>{@code DataSource}
+ * <li>{@code OrmConversionServiceConfig} - optional for registering custom
+ * converters.
  * 
  * @since 1.72.0
  * @author Razvan Popian
